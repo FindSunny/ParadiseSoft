83c47f63424182180ee2663d11b1db94d6973ad9
Findfly <Findfly@192.168.1.105>
2016-04-10 22:57:48 +0800

    edit events & debug

--- js/menu.js
+++ js/menu.js
@@ -30,13 +30,55 @@ mui.init({
 mui.plusReady(function() {
 	console.log("当前页面URL："+plus.webview.currentWebview().getURL());
 	
-	//tap为mui封装的单击事件
+	//交通费用
 	document.getElementById('traffic').addEventListener('tap', function() {
-	  //打开关于页面
+		openWindow();
+	});
+	
+	//住宿费用
+	document.getElementById('shelter').addEventListener('tap', function() {
+		openWindow();
+	});
+	
+	//三餐费用
+	document.getElementById('food').addEventListener('tap', function() {
+		openWindow();
+	});
+	
+	//玩乐费用
+	document.getElementById('fun').addEventListener('tap', function() {
+		openWindow();
+	});
+
+	
+});
+
+function openWindow(){
+	//打开关于页面
 	  mui.openWindow({
 	    url: '../pages/addBill.html', 
 	    id:'info'
 	  });
-	});
-	
-});
+}
+
+//首页返回键处理
+//处理逻辑：1秒内，连续两次按返回键，则退出应用；
+var first = null;
+mui.back = function() {
+	if (showMenu) {
+		closeMenu();
+	} else {
+		//首次按键，提示‘再按一次退出应用’
+		if (!first) {
+			first = new Date().getTime();
+			mui.toast('再按一次退出应用');
+			setTimeout(function() {
+				first = null;
+			}, 1000);
+		} else {
+			if (new Date().getTime() - first < 1000) {
+				plus.runtime.quit();
+			}
+		}
+	}
+};
